e279e469979bc51c1ac0cd0cd06f840cb08c3f40
Emmanuel <emmanuelayofame@gmail.com>
2025-12-22 14:32:18 +0500

    home

--- Ecomerce/src/components/Home.tsx
+++ Ecomerce/src/components/Home.tsx
@@ -1,12 +1,20 @@
 import Sidebar from "../components/Sidebar/Sidebar";
 import Banner from "../components/Banner/Banner";
+import FlashSales from "./flashsales/flashsales";
 
 const Home = () => {
   return (
+    <>
     <div style={{ display: "flex", gap: "24px" }}>
       <Sidebar />
       <Banner />
+      
+
     </div>
+    <div>
+      <FlashSales/>
+    </div>
+    </>
   )
 }
 
