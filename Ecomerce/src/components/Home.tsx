a19f3abbe6b86d00b0b3672f0feedeb719a5a2d5
Emmanuel <emmanuelayofame@gmail.com>
2025-12-22 14:32:18 +0500

    home

--- Ecomerce/src/components/Home.tsx
+++ Ecomerce/src/components/Home.tsx
@@ -6,27 +6,30 @@ import Categories from "../components/Category/Categories";
 import BestSellingProducts from "./Banner2/BestSellingProducts";
 import Navbar from "../components/Navbar/Navbar";
 
-
 const Home = () => {
   return (
     <>
       <div>
         <Header />
       </div>
+
       <div>
         <Navbar />
       </div>
+
       <div style={{ display: "flex", gap: "24px" }}>
         <Sidebar />
         <Banner />
       </div>
+
       <div>
         <Categories />
       </div>
+
       <div>
         <BestSellingProducts />
       </div>
-      
+
       <div>
         <Footer />
       </div>
--- Ecomerce/src/components/flashsales/flashsales.tsx
+++ Ecomerce/src/components/flashsales/flashsales.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./flashsales.css"
 
 const FlashSales = () => {
   return (
