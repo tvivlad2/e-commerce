8e771344d67d1624488bfd307002246f1f2cbff3
EmmanuelFame <emmanuelayofame@gmail.com>
2026-01-18 23:57:11 +0500

    chore: initial header component setup

--- Ecomerce/src/components/Home.tsx
+++ Ecomerce/src/components/Home.tsx
@@ -1,40 +1,21 @@
 import Sidebar from "../components/Sidebar/Sidebar";
 import Banner from "../components/Banner/Banner";
 import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
-import Categories from "../components/Category/Categories";
-import BestSellingProducts from "./Banner2/BestSellingProducts";
-import Navbar from "../components/Navbar/Navbar";
-
+import Footer from "./Footer";
 const Home = () => {
   return (
     <>
       <div>
         <Header />
       </div>
-
-      <div>
-        <Navbar />
-      </div>
-
       <div style={{ display: "flex", gap: "24px" }}>
         <Sidebar />
         <Banner />
       </div>
-
-      <div>
-        <Categories />
-      </div>
-
-      <div>
-        <BestSellingProducts />
-      </div>
-
       <div>
         <Footer />
       </div>
     </>
   );
-};
-
-export default Home;
+}
+export default Home
